19df49da79d90510073eee15f02218e8e4e34c36
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-05-20 16:06:04 +0500

    base information

---
+++ constants/comicsInfo.tsx
@@ -0,0 +1,30 @@
+import jabrmaqom from '@public/jabrmaqom.jpg'
+import olmayman from '@public/olmayman.jpg'
+import eraPrimo from '@public/eraPrimo.jpg'
+
+export const comicksInfo = [
+    {
+        id:1,
+        comicName:'O`lmayman',
+        page:1,
+        allPage:1,
+        img:olmayman,
+        comicFile:''
+    },
+    {
+        id:2,
+        comicName:'Jabrmaqom',
+        page:10,
+        allPage:'?',
+        img:jabrmaqom,
+        comicFile:''
+    },
+    {
+        id:3,
+        comicName:'Era Primo',
+        page:1,
+        allPage:'?',
+        img:eraPrimo,
+        comicFile:''
+    },
+]
