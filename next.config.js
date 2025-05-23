c73bf0a4d624f874552e654ca2fc8e4c54c06b4f
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-05-23 10:51:12 +0500

    add language

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    i18n: {
+      locales: ['en', 'uz', 'ru'], // qo'llab-quvvatlanadigan tillar
+      defaultLocale: 'en', // standart til
+    },
+  };
+  
+  module.exports = nextConfig;
+  
